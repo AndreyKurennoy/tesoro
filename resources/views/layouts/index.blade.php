6d66e3363a7b2100156980ab068cca2223cd87b1
AndreyKurennoy <19ofis96@mail.ru>
2018-05-12 19:26:33 +0300

    adding scss files, libs, fonts

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+</head>
+<body>
+    @extends('layouts.header')
+</body>
+</html>
