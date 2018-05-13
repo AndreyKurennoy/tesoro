9398cf5d992d0ca2ea3c65324aa60cbcacd393e7
AndreyKurennoy <19ofis96@mail.ru>
2018-05-13 21:07:06 +0300

    adding scss files, libs, fonts

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,8 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css/app.css">
+    {{--<link rel="stylesheet" href="./resources/assets/sass/app.scss">--}}
 </head>
 <body>
     @extends('layouts.header')
+    <script src="/js/app.js"></script>
 </body>
 </html>
