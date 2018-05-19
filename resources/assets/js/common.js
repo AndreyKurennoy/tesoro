fd301a178c207e91ff48643734b5454041b28b61
Oleh <oleg.gashenko@gmail.com>
2018-05-19 15:35:26 +0300

    header without submenu

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,209 +1,255 @@
 $(function() {
+  $('header .hamburger').click(function() {
+    $('.header__modal').addClass('show');
+  });
+  $('.header__modal-overlay').click(function() {
+    $('.header__modal').removeClass('show');
+  });
 
-    function productSlider() {
-        if ($(".section--product-slider").length > 0) {
-            $('.product-slider__tabgroup > div').hide();
-            $('.product-slider__tabgroup> div:first-of-type').show();
-            $('.product-slider__tabs-list>li>a').click(function(e) {
-                e.preventDefault();
-                var $this = $(this);
-                var tabgroup = '#' + $this.parents('.product-slider__tabs-list').data('tabgroup');
-                var others = $this.closest('li').siblings().children('a');
-                var target = $this.attr('href');
-                others.removeClass('active');
-                $this.addClass('active');
-                $(tabgroup).children('.product-slider__tabgroup-item').hide();
-                $(target).show();
-
-            });
-        }
-    };
-    productSlider();
-
-
-
-
-
-    if ($(".section--product-slider").length > 0) {
-        $(".product-slider__tabgroup-item").owlCarousel({
-            items: 6,
-            margin: 10,
-            nav: true,
-            dots: true,
-            loop: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-            responsive: {
-                320: {
-                    items: 2,
-
-                },
-                768: {
-                    items: 3,
-
-                },
-                1280: {
-                    items: 4,
-
-                },
-                1366: {
-                    items: 5,
-
-                },
-                1600: {
-                    items: 6
-
-                }
-
-            }
-        });
+  function startCarouselHeader() {
+    if ($('.header').length > 0) {
+      $('.header__bottom-menu').owlCarousel({
+        items: 4,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+          425: {
+            items: 5,
+          },
+          600: {
+            items: 7,
+          },
+          768: {
+            items: 8,
+          },
+          920: {
+            items: 9,
+          },
+          1280: {
+            items: 10,
+          },
+        },
+      });
     }
-
-    if ($(".section--banner").length > 0) {
-        $(".banner").owlCarousel({
-            items: 1,
-            margin: 10,
-            nav: false,
-            dots: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-        });
+  }
+  function stopCarouselHeader() {
+    var owl = $('.header__bottom-menu');
+    owl.trigger('destroy.owl.carousel');
+    owl.addClass('off');
+  }
+  $(document).ready(function() {
+    if ($(window).width() < 1280) {
+      startCarouselHeader();
+    } else {
+      $('.header__bottom-menu').addClass('off');
     }
+  });
 
-
-
-    function startCarousel() {
-        if ($(".section-promo").length > 0) {
-            $(".promo").owlCarousel({
-                items: 1,
-                margin: 10,
-                dots: true,
-                loop: true,
-
-            });
-        }
-        if ($(".section--advantage").length > 0) {
-            $(".advantage").owlCarousel({
-                items: 3,
-                margin: 10,
-                dots: true,
-                loop: true,
-
-            });
-        }
+  $(window).resize(function() {
+    if ($(window).width() < 1280) {
+      startCarouselHeader();
+    } else {
+      stopCarouselHeader();
     }
+  });
 
-
-
-    function stopCarousel() {
-        var owl = $('.promo, .advantage');
-        owl.trigger('destroy.owl.carousel');
-        owl.addClass('off');
+  function productSlider() {
+    if ($('.section--product-slider').length > 0) {
+      $('.product-slider__tabgroup > div').hide();
+      $('.product-slider__tabgroup> div:first-of-type').show();
+      $('.product-slider__tabs-list>li>a').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var tabgroup = '#' + $this.parents('.product-slider__tabs-list').data('tabgroup');
+        var others = $this
+          .closest('li')
+          .siblings()
+          .children('a');
+        var target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup)
+          .children('.product-slider__tabgroup-item')
+          .hide();
+        $(target).show();
+      });
     }
+  }
+  productSlider();
 
-
-    $(document).ready(function() {
-        if ($(window).width() < 480) {
-            startCarousel();
-        } else {
-            $('.promo, .advantage').addClass('off');
-        }
+  if ($('.section--product-slider').length > 0) {
+    $('.product-slider__tabgroup-item').owlCarousel({
+      items: 6,
+      margin: 10,
+      nav: true,
+      dots: true,
+      loop: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        320: {
+          items: 2,
+        },
+        768: {
+          items: 3,
+        },
+        1280: {
+          items: 4,
+        },
+        1366: {
+          items: 5,
+        },
+        1600: {
+          items: 6,
+        },
+      },
     });
+  }
 
-    $(window).resize(function() {
-        if ($(window).width() < 480) {
-            startCarousel();
-        } else {
-            stopCarousel();
-        }
+  if ($('.section--banner').length > 0) {
+    $('.banner').owlCarousel({
+      items: 1,
+      margin: 10,
+      nav: false,
+      dots: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
     });
+  }
 
-
-
-    if ($(".section--brands").length > 0) {
-        $(".brands").owlCarousel({
-            items: 3,
-            margin: 30,
-            nav: true,
-            dots: true,
-            stagePadding: 0,
-            loop: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-            responsive: {
-
-                1280: {
-                    items: 4,
-                    stagePadding: 80,
-
-
-
-                },
-                1366: {
-                    items: 5,
-                    margin: 20,
-
-                },
-                1600: {
-                    items: 6,
-                    margin: 20,
-
-                }
-
-            }
-        });
+  function startCarousel() {
+    if ($('.section-promo').length > 0) {
+      $('.promo').owlCarousel({
+        items: 1,
+        margin: 10,
+        dots: true,
+        loop: true,
+      });
     }
-    if ($(".section--instagram").length > 0) {
-        $(".instagram").owlCarousel({
-            items: 3,
-            margin: 15,
-            nav: true,
-            dots: true,
-            stagePadding: 0,
-            loop: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-            responsive: {
-                480: {
-                    stagePadding: 20,
-                },
-                768: {
-                    items: 4,
-
-                    stagePadding: 30,
-                },
-                1280: {
-                    items: 4,
-                    stagePadding: 120,
-
-                },
-                1366: {
-                    items: 5,
-                    stagePadding: 120,
-
-                },
-                1600: {
-                    items: 6,
-
-                    stagePadding: 120,
-                }
-
-            }
-        });
+    if ($('.section--advantage').length > 0) {
+      $('.advantage').owlCarousel({
+        items: 3,
+        margin: 10,
+        dots: true,
+        loop: true,
+      });
     }
-    if ($(".section--news-review").length > 0) {
-        $(".news").owlCarousel({
-            items: 1,
-            margin: 10,
-            nav: true,
-            dots: false,
-            loop: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-        });
-        $(".review").owlCarousel({
-            items: 1,
-            margin: 10,
-            nav: true,
-            dots: false,
-            loop: true,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>'],
-        });
-    }
+  }
 
+  function stopCarousel() {
+    var owl = $('.promo, .advantage');
+    owl.trigger('destroy.owl.carousel');
+    owl.addClass('off');
+  }
+
+  $(document).ready(function() {
+    if ($(window).width() < 480) {
+      startCarousel();
+    } else {
+      $('.promo, .advantage').addClass('off');
+    }
+  });
+
+  $(window).resize(function() {
+    if ($(window).width() < 480) {
+      startCarousel();
+    } else {
+      stopCarousel();
+    }
+  });
+
+  if ($('.section--brands').length > 0) {
+    $('.brands').owlCarousel({
+      items: 3,
+      margin: 30,
+      nav: true,
+      dots: true,
+      stagePadding: 0,
+      loop: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        1280: {
+          items: 4,
+          stagePadding: 80,
+        },
+        1366: {
+          items: 5,
+          margin: 20,
+        },
+        1600: {
+          items: 6,
+          margin: 20,
+        },
+      },
+    });
+  }
+  if ($('.section--instagram').length > 0) {
+    $('.instagram').owlCarousel({
+      items: 3,
+      margin: 15,
+      nav: true,
+      dots: true,
+      stagePadding: 0,
+      loop: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        480: {
+          stagePadding: 20,
+        },
+        768: {
+          items: 4,
+
+          stagePadding: 30,
+        },
+        1280: {
+          items: 4,
+          stagePadding: 120,
+        },
+        1366: {
+          items: 5,
+          stagePadding: 120,
+        },
+        1600: {
+          items: 6,
+
+          stagePadding: 120,
+        },
+      },
+    });
+  }
+  if ($('.section--news-review').length > 0) {
+    $('.news').owlCarousel({
+      items: 1,
+      margin: 10,
+      nav: true,
+      dots: false,
+      loop: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
+    });
+    $('.review').owlCarousel({
+      items: 1,
+      margin: 10,
+      nav: true,
+      dots: false,
+      loop: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2" aria-hidden="true"></i>',
+      ],
+    });
+  }
 });
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,81 +1,235 @@
 <header class="header">
-    <div class="header__top">
-        <nav class="header__top-left">
-            <ul class="header__top-menu">
-                <li><a href="#">Новости</a></li>
-                <li><a href="#">О нас</a></li>
-                <li><a href="#">Доставка и оплата</a></li>
-                <li><a href="#">Гарантия</a>
-                    <ul class="header__top-submenu">
-                        <li><a href="#">Пункт 1</a></li>
-                        <li><a href="#">Пункт 1</a></li>
-                        <li><a href="#">Пункт 1</a></li>
-                        <li><a href="#">Пункт 1</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Возврат и обмен</a></li>
-                <li><a href="#">Наши магазины</a></li>
-                <li><a href="#">Контакты</a></li>
-                <li><a href="#">Наши бренды</a></li>
-                <li><a href="#">Отзывы и предложения</a></li>
-                <li><a href="#">Программа лояльности</a></li>
-            </ul>
-        </nav>
-        <div class="header__top-right">
-            <div class="header__top-lang"><a href="">Рус</a><span>|</span><a href="#">Укр</a></div>
-            <div class="header__top-login">
-                <a href="#">Вход/Регистрация</a>
+        <div class="header__modal">
+            <div class="header__modal-overlay"></div>
+            <div class="header__modal-menu">
+                <ul class="header__modal-menu-items">
+                    <li>
+                        <a href="#">Доставка и оплата</a>
+                    </li>
+                    <li>
+                        <a href="#">Гарантия</a>
+                    </li>
+                    <li>
+                        <a href="#">Наши магазины</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                    <li>
+                        <a href="#">Наши бренды</a>
+                    </li>
+                    <li>
+                        <a href="#">Отзывы и предложения</a>
+                    </li>
+                    <li>
+                        <a href="#">Программа лояльности</a>
+                    </li>
+                    <li>
+                        <a href="#">Новости</a>
+                    </li>
+                    <li>
+                        <a href="#">О нас</a>
+                    </li>
+
+                </ul>
+                <ul class="header__modal-menu-social_lang">
+                    <li>
+                        <span>Мы в соц.сетях</span>
+                        <a href="#">
+                            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                            </i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <span>Язык</span>
+                        <a href="#">
+                            Рус
+                        </a>
+                        <a href="#">
+                            Укр
+                        </a>
+                    </li>
+                </ul>
+                <ul class="header__modal-menu-func">
+                    <li>
+                        <a href="#">Каталог</a>
+                    </li>
+                    <li>
+                        <a href="#">Подбор</a>
+                    </li>
+                    <li>
+                        <a href="#">Обратный звонок</a>
+                    </li>
+                    <li>
+                        <a href="#">Вход/Регистрация</a>
+                    </li>
+                </ul>
+
             </div>
-            <div class="header__top-social">
-                <a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></i></a>
-                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+
+        </div>
+        <div class="header__top">
+            <nav class="header__top-left">
+                <ul class="header__top-menu">
+                    <li>
+                        <a href="#">Новости</a>
+                    </li>
+                    <li>
+                        <a href="#">О нас</a>
+                    </li>
+                    <li>
+                        <a href="#">Доставка и оплата</a>
+                    </li>
+                    <li>
+                        <a href="#">Гарантия</a>
+                        <ul class="header__top-submenu">
+                            <li>
+                                <a href="#">Пункт 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Пункт 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Пункт 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Пункт 1</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Возврат и обмен</a>
+                    </li>
+                    <li>
+                        <a href="#">Наши магазины</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                    <li>
+                        <a href="#">Наши бренды</a>
+                    </li>
+                    <li>
+                        <a href="#">Отзывы и предложения</a>
+                    </li>
+                    <li>
+                        <a href="#">Программа лояльности</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__top-right">
+                <div class="header__top-lang">
+                    <a href="">Рус</a>
+                    <span>|</span>
+                    <a href="#">Укр</a>
+                </div>
+                <div class="header__top-login">
+                    <a href="#">Вход/Регистрация</a>
+                </div>
+                <div class="header__top-social">
+                    <a href="#">
+                        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                        </i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="header__middle">
-        <div class="header__middle-mobile">
-            <a href="#" class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></a>
-        </div>
-        <div class="header__middle-left">
-            <div class="header__middle-logo">
-                <a href="">
-                    <img src="/img/logo.png" alt="" class="img-responsive">
+        <div class="header__middle">
+            <div class="header__middle-mobile">
+                <a href="#" class="hamburger">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
                 </a>
             </div>
-            <div class="header__middle-funcset">
-                <a href="#"><i class="fa fa-list" aria-hidden="true"></i><span>Каталог</span></a>
-                <a href="#"><i class="fa fa-align-center" aria-hidden="true"></i><span>Подбор</span></a>
-                <a href="#"><i class="fa fa-search" aria-hidden="true"></i><span>Поиск товара</span></a>
+            <div class="header__middle-left">
+                <div class="header__middle-logo">
+                    <a href="#">
+                        <img src="img/Logo.png" alt="" class="img-responsive">
+                    </a>
+                </div>
+                <div class="header__middle-funcset">
+                    <a class="header__middle-funcset-catalog" href="#">
+                        <i class="fa fa-list" aria-hidden="true"></i>
+                        <span>Каталог</span>
+                    </a>
+                    <a class="header__middle-funcset-match" href="#">
+                        <i class="fa fa-align-center" aria-hidden="true"></i>
+                        <span>Подбор</span>
+                    </a>
+                    <a class="header__middle-funcset-search" href="#">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        <span>Поиск товара</span>
+                    </a>
+                </div>
+            </div>
+            <div class="header__middle-right">
+                <div class="header__middle-tell">
+                    <a href="tel:+1234567890">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <span>(063) 234 59 99</span>
+                        <p>Обратный звонок</p>
+                    </a>
+                </div>
+                <div class="header__middle-address">
+                    <a href="#">Адрес
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="header__middle-cart">
+                    <a href="#">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        <span>Корзина пуста</span>
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="header__middle-right">
-            <div class="header__middle-tell"><a href="tel:+1234567890"><i class="fa fa-phone" aria-hidden="true"></i><span>(063) 234 59 99</span><p>Обратный звонок</p></a></div>
-            <div class="header__middle-address"><a href="#">Адрес<i class="fa fa-angle-down" aria-hidden="true"></i></a></div>
-            <div class="header__middle-cart"><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Корзина пуста</span></a></div>
-        </div>
-    </div>
-    <nav class="header__bottom">
-        <ul class="header__bottom-menu">
-            <li><a href="#">бренды</a></li>
-            <li><a href="#">кольца</a></li>
-            <li><a href="#">серьги</a></li>
-            <li><a href="#">подвесы</a></li>
-            <li><a href="#">цепи</a></li>
-            <li><a href="#">браслеты</a></li>
-            <li><a href="#">звенья nomination</a></li>
-            <li><a href="#">часы</a>
-                <ul class="header__bottom-submenu">
-                    <li><a href="#">Мужские</a></li>
-                    <li><a href="#">Женские</a></li>
-                </ul>
-            </li>
-            <li><a href="#">сумки</a></li>
-            <li><a href="#">аксессуары для мужчин</a></li>
-            <li><a href="#">золотые украшения</a></li>
-            <li><a href="#">серебрянные украшения</a></li>
-        </ul>
-    </nav>
-</header>
+        <nav class="header__bottom">
+            <ul class="header__bottom-menu owl-carousel owl-theme">
+                <li>
+                    <a href="#">бренды</a>
+                </li>
+                <li>
+                    <a href="#">кольца</a>
+                </li>
+                <li>
+                    <a href="#">серьги</a>
+                </li>
+                <li>
+                    <a href="#">подвесы</a>
+                </li>
+                <li>
+                    <a href="#">цепи</a>
+                </li>
+                <li>
+                    <a href="#">браслеты</a>
+                </li>
+                <li>
+                    <a href="#">звенья nomination</a>
+                </li>
+                <li>
+                    <a href="#">часы</a>
+
+                </li>
+                <li>
+                    <a href="#">сумки</a>
+                </li>
+                <li>
+                    <a href="#">аксессуары для мужчин</a>
+                </li>
+                <li>
+                    <a href="#">золотые украшения</a>
+                </li>
+                <li>
+                    <a href="#">серебрянные украшения</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 <section class="section section--banner">
         <div class="banner owl-carousel owl-theme">
             <div class="banner__slide" style="background-image: url(http://tesoro-jewelry.com.ua/images/photos_main/11/photo.jpg);"></div>
